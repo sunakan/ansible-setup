90e461bd1ef548631c6c28c1aed1fc7fb005b80c
sunakan <sunakan03@gmail.com>
2024-08-12 22:49:07 +0900

    rswagの設定

--- app/backend-rails-sidekiq/spec/swagger_helper.rb
+++ app/backend-rails-sidekiq/spec/swagger_helper.rb
@@ -3,17 +3,7 @@
 require 'rails_helper'
 
 RSpec.configure do |config|
-  # Specify a root folder where Swagger JSON files are generated
-  # NOTE: If you're using the rswag-api to serve API descriptions, you'll need
-  # to ensure that it's configured to serve Swagger from the same folder
   config.openapi_root = Rails.root.join('swagger').to_s
-
-  # Define one or more Swagger documents and provide global metadata for each one
-  # When you run the 'rswag:specs:swaggerize' rake task, the complete Swagger will
-  # be generated at the provided relative path under openapi_root
-  # By default, the operations defined in spec files are added to the first
-  # document below. You can override this behavior by adding a openapi_spec tag to the
-  # the root example_group in your specs, e.g. describe '...', openapi_spec: 'v2/swagger.json'
   config.openapi_specs = {
     'v1/swagger.yaml' => {
       openapi: '3.0.1',
@@ -34,10 +24,10 @@ RSpec.configure do |config|
       ]
     }
   }
-
-  # Specify the format of the output Swagger file when running 'rswag:specs:swaggerize'.
-  # The openapi_specs configuration option has the filename including format in
-  # the key, this may want to be changed to avoid putting yaml in json files.
-  # Defaults to json. Accepts ':json' and ':yaml'.
   config.openapi_format = :yaml
+
+  # responseに未知のプロパティが含まれる場合の挙動
+  # strictにするかしないか
+  # default: false(PASSする)
+  config.openapi_strict_schema_validation = true
 end

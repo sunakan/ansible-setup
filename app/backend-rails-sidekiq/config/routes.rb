67e4fff039dacaae27ec12444c1fa444bc05837f
sunakan <sunakan03@gmail.com>
2024-08-12 22:42:04 +0900

    [cmd] bundle exec rubocop -a

--- app/backend-rails-sidekiq/config/routes.rb
+++ app/backend-rails-sidekiq/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
   root "welcome#show"
 end

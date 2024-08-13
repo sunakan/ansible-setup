c00c6ecfe4e566af5cf3c0018764f516e18577ba
sunakan <sunakan03@gmail.com>
2024-08-13 20:36:32 +0900

    tmp

--- app/frontend-rails/app/controllers/welcome_controller.rb
+++ app/frontend-rails/app/controllers/welcome_controller.rb
@@ -1,13 +1,11 @@
 require 'net/http'
 class WelcomeController < ApplicationController
   def show
-    response = Net::HTTP.get_response(URI("#{api_endpoint}/"))
+    response = Net::HTTP.get(
+      URI("#{api_endpoint}/"),
+      { 'Accept' => 'application/json' }
+    )
 
-    case response
-    when Net::HTTPSuccess
-      render :show, locals: { response: JSON.parse(response.body) }
-    else
-      raise "Unexpected response: #{response}"
-    end
+    render :show, locals: { response: JSON.parse(response) }
   end
 end

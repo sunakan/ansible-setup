3789c402e2dcdfa99590b1b613dc41e7bcb868e9
sunakan <sunakan03@gmail.com>
2024-08-13 17:44:51 +0900

    tmp

--- app/frontend-rails/app/controllers/application_controller.rb
+++ app/frontend-rails/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def api_endpoint
+    "http://localhost:3000"
+  end
 end
--- app/frontend-rails/app/controllers/welcome_controller.rb
+++ app/frontend-rails/app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
+require 'net/http'
 class WelcomeController < ApplicationController
   def show
+    response = Net::HTTP.get_response(URI("#{api_endpoint}/"))
+
+    case response
+    when Net::HTTPSuccess
+      render :show, locals: { response: JSON.parse(response.body) }
+    else
+      raise "Unexpected response: #{response}"
+    end
   end
 end

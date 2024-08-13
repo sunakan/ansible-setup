2fa54d81408fea0799b614dfc37f4a06d54888b0
sunakan <sunakan03@gmail.com>
2024-08-13 13:03:30 +0900

    layoutを追加

---
+++ app/frontend-sveltekit/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<main>
+    <header>
+        <nav class="bg-black text-white px-4 py-4 flex items-center">
+            <h1 class="text-xl uppercase tracking-tight font-medium">
+                <a href="/">
+                    APP
+                </a>
+            </h1>
+            <a class="text-sm hover:text-gray-200 font-light hover:underline ml-7 " href="/orders/new">
+                Orderレコードの作成 →
+            </a>
+        </nav>
+    </header>
+    <slot />
+</main>
+
+<script lang="ts">
+  import "../app.css";
+</script>

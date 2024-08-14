027246723093d32f2044d75cd51226ad722e4974
sunakan <sunakan03@gmail.com>
2024-08-14 12:30:47 +0900

    tmp

---
+++ app/frontend-rails/app/controllers/orders_controller.rb
@@ -0,0 +1,49 @@
+class OrdersController < ApplicationController
+  def new
+    response = Net::HTTP.get(
+      URI("#{api_endpoint}/orders/new"),
+      { 'Accept' => 'application/json' }
+    )
+
+    render :new, locals: { response: JSON.parse(response).merge({'authenticity_token' => form_authenticity_token}) }
+  end
+
+  def create
+    permitted = params.require(:order).permit(:product_id, :email, :address, :quantity)
+    response = Net::HTTP.post(
+      URI("#{api_endpoint}/orders/"),
+      permitted.to_json,
+      {
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json',
+      }
+    )
+
+    case response
+    when Net::HTTPOK
+      order = JSON.parse(response.body)
+      redirect_to "/orders/#{order['id']}"
+      return
+    when Net::HTTPBadRequest
+      render :new, locals: { response: JSON.parse(response.body) }
+    else
+      raise "Unexpected response: #{response.inspect}"
+    end
+  end
+
+  def show
+    response = Net::HTTP.get_response(
+      URI("#{api_endpoint}/orders/#{params[:id]}"),
+      { 'Accept' => 'application/json' }
+    )
+
+    case response
+    when Net::HTTPOK
+      render :show, locals: { response: JSON.parse(response.body) }
+    when Net::HTTPNotFound
+      render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+    else
+      raise "Unexpected response: #{response.inspect}"
+    end
+  end
+end
--- app/frontend-rails/config/routes.rb
+++ app/frontend-rails/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :orders, only: [ :new, :create, :show ]
+  # todo: simulated behaviorsの実装
   root 'welcome#show'
 end
